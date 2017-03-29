e3954dfe001776ae62e0db943c86ebe633b52957
Carles Figuerola <Carles.Figuerola@orbitz.com>
2017-03-29 09:04:05 -0700

    sys_id to sysid on update and delete

--- actions/update.py
+++ actions/update.py
@@ -3,6 +3,6 @@ from lib.actions import BaseAction
 
 class UpdateAction(BaseAction):
     def run(self, table, payload, sysid):
-        r = self.client.query(table=table, query={'sys_id': sys_id}).get_one() # pylint: disable=no-member
+        r = self.client.query(table=table, query={'sys_id': sysid}).get_one() # pylint: disable=no-member
         response = r.update(payload)  # pylint: disable=no-member
         return response
