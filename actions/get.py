f850e330efdecae9dff596942146612d3e1bf84c
Carles Figuerola <Carles.Figuerola@orbitz.com>
2017-03-29 09:56:27 -0700

    fix more errors caught with circleci

--- actions/get.py
+++ actions/get.py
@@ -4,8 +4,8 @@ from lib.actions import BaseAction
 class GetAction(BaseAction):
     def run(self, table, query):
         r = self.client.query(table=table, query=query)  # pylint: disable=no-member
-        response = self.client.get_all()  # pylint: disable=no-member
+        response = r.get_all()  # pylint: disable=no-member
         output = []
         for each_item in response:
-          output.append(each_item)
+            output.append(each_item)
         return output
--- actions/get_non_structured.py
+++ actions/get_non_structured.py
@@ -4,8 +4,8 @@ from lib.actions import BaseAction
 class GetNonStructuredAction(BaseAction):
     def run(self, table, query):
         r = self.client.query(table=table, query=str(query))  # pylint: disable=no-member
-        response = self.client.get_all()  # pylint: disable=no-member
+        response = r.get_all()  # pylint: disable=no-member
         output = []
         for each_item in response:
-          output.append(each_item)
+            output.append(each_item)
         return output
