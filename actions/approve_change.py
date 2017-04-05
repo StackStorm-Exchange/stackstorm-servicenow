20803f31cf4712d495d162f9ed0aa74db6e1dc83
Carles Figuerola <carles@figuerola.info>
2017-04-05 13:12:32 -0700

    We don't need to get_one() to update content

--- actions/approve_change.py
+++ actions/approve_change.py
@@ -4,6 +4,6 @@ from lib.actions import BaseAction
 class ApprovalAction(BaseAction):
     def run(self, number):
         s = self.client
-        r = s.query(table='change_request', query={'number': number}).get_one()
+        r = s.query(table='change_request', query={'number': number})
         response = r.update({'approval': 'approved'})
         return response
--- actions/update.py
+++ actions/update.py
@@ -4,6 +4,6 @@ from lib.actions import BaseAction
 class UpdateAction(BaseAction):
     def run(self, table, payload, sysid):
         s = self.client
-        r = s.query(table=table, query={'sys_id': sysid}).get_one()  # pylint: disable=no-member
+        r = s.query(table=table, query={'sys_id': sysid})  # pylint: disable=no-member
         response = r.update(payload)  # pylint: disable=no-member
         return response
